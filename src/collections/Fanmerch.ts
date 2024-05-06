7c5f71847edca76cbacf46b2c8843e919383f91c
Daniils Petrovs <daniils@platogo.com>
2024-05-06 23:58:17 +0200

    feat: add Fanmerch collection

---
+++ src/collections/Fanmerch.ts
@@ -0,0 +1,73 @@
+import type { CollectionConfig } from 'payload/types';
+import checkSection from 'lib/checkSection';
+
+const Fanmerch: CollectionConfig = {
+	slug: 'fanmerch',
+	labels: {
+		singular: 'Fanmerch',
+		plural: 'Fanmerch',
+	},
+	admin: {
+		useAsTitle: 'name',
+		defaultColumns: ['name', 'price', 'quantity', 'category'],
+		hidden: (req) => !checkSection(req, 'dokomi-fan-booth'),
+	},
+	access: {
+		read: () => true,
+		create: ({ req }) => checkSection(req, 'dokomi-fan-booth'),
+		update: ({ req }) => checkSection(req, 'dokomi-fan-booth'),
+		delete: ({ req }) => checkSection(req, 'dokomi-fan-booth'),
+	},
+	fields: [
+		{
+			name: 'name',
+			type: 'text',
+			required: true,
+		},
+		{
+			name: 'description',
+			type: 'textarea',
+			required: false,
+			maxLength: 500,
+		},
+		{
+			name: 'image',
+			type: 'relationship',
+			relationTo: 'media',
+			required: false,
+		},
+		{
+			name: 'price',
+			type: 'number',
+			required: false,
+			min: 0,
+		},
+		{
+			name: 'quantity',
+			type: 'number',
+			required: false,
+			defaultValue: 0,
+			min: 0,
+		},
+		{
+			name: 'category',
+			type: 'select',
+			options: ['button', 'postcard', 'keychain', 'sticker', 'other'],
+			required: true,
+		},
+		{
+			name: 'model',
+			type: 'text',
+			admin: {
+				description: 'An URL to the 3D model file',
+			},
+		},
+		{
+			name: 'project',
+			type: 'relationship',
+			relationTo: 'project',
+		},
+	],
+};
+
+export default Fanmerch;
