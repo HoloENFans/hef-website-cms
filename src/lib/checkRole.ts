2a7905267e32fced85a304da772ca19be10b2fbb
GoldElysium <goldelysium@gmail.com>
2024-06-18 16:11:46 +0200

    fix: bypass checks for local API calls

--- src/lib/checkRole.ts
+++ src/lib/checkRole.ts
@@ -1,7 +1,9 @@
 import { PayloadRequest } from 'payload/dist/express/types';
 import { User } from 'payload/auth';
 
-export default ({ user }: PayloadRequest | { user: User }, role: string | string[]) => {
+export default ({ user, payloadAPI }: PayloadRequest & { user: User }, role: string | string[]) => {
+	if (payloadAPI === 'local') return true;
+
 	if ((user.roles as string[]).includes('superadmin')) return true;
 
 	if (role instanceof Array) {
--- src/lib/checkSection.ts
+++ src/lib/checkSection.ts
@@ -1,7 +1,9 @@
 import { PayloadRequest } from 'payload/dist/express/types';
 import { User } from 'payload/auth';
 
-export default ({ user }: PayloadRequest | { user: User }, section: string) => {
+export default ({ user, payloadAPI }: PayloadRequest & { user: User }, section: string) => {
+	if (payloadAPI === 'local') return true;
+
 	if ((user.roles as string[]).includes('superadmin')) return true;
 
 	return (user.sections as string[]).includes(section);
