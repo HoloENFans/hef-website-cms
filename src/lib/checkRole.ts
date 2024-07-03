9d548b0128077ce56b7983a21234d6139c6f955f
GoldElysium <goldelysium@gmail.com>
2024-07-03 19:38:23 +0200

    fix: check if user is set

--- src/lib/checkRole.ts
+++ src/lib/checkRole.ts
@@ -3,12 +3,21 @@ import { User } from 'payload/auth';
 export default ({ user }: { user: User }, role: string | string[]) => {
 	if (process.env.PAYLOAD_MIGRATING === 'true') return true;
 
-	if ((user.roles as string[]).includes('superadmin')) return true;
+	if (!user) return false;
 
-	if (role instanceof Array) {
-		const map = role.map((roleName) => (user.roles as string[]).includes(roleName));
-		return map.includes(true);
+	try {
+		if ((user.roles as string[]).includes('superadmin')) return true;
+
+		if (role instanceof Array) {
+			const map = role.map((roleName) => (user.roles as string[]).includes(roleName));
+			return map.includes(true);
+		}
+
+		return (user.roles as string[]).includes(role);
+	} catch (e) {
+		console.error(e);
+		console.error('Failed to check the following user: ', user);
+
+		return false;
 	}
-
-	return (user.roles as string[]).includes(role);
 };
--- src/lib/checkSection.ts
+++ src/lib/checkSection.ts
@@ -1,6 +1,8 @@
 import { User } from 'payload/auth';
 
 export default ({ user }: { user: User }, section: string) => {
+	if (!user) return false;
+
 	if (process.env.PAYLOAD_MIGRATING === 'true') return true;
 
 	if ((user.roles as string[]).includes('superadmin')) return true;
